1c1e017e310afc6811f089eb60c11c6ac87d481b
IrtisumMusharratRahman <rhythmfakir@gmail.com>
2023-01-22 18:40:10 +0600

    Splash Screen Integration

--- app/src/main/java/com/example/algovisual/MainActivity.kt
+++ app/src/main/java/com/example/algovisual/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.algovisual.route.NavScreens
@@ -31,6 +32,7 @@ import kotlinx.coroutines.launch
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             AlgoVisualTheme {
                 MainScreen()
